4dbb456407883a4f220f893a6e456d374e970629
Chris Tully <christully12@gmail.com>
2024-11-18 16:07:51 -0500

    Added router package. Added site footer/header/logo in this commit. Just placeholder code

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HomePage msg="Welcome to Your Vue.js App" />
+  <h1>Hello App!</h1>
+  <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
+  <!-- <nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/about">Go to About</RouterLink>
+  </nav> -->
+  <main>
+    <RouterView />
+  </main>
 </template>
-
 <script>
 import HomePage from "./views/HomePage/HomePage.vue";
+import TopHeader from "./components/SiteHeader/TopHEader.vue";
 
 export default {
   name: "App",
---
+++ src/components/SiteHeader/SiteHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    SiteHeader
+    <!-- <RouterLink
+      v-for="(item, index) in linkCollection"
+      :key="index"
+      :to="item.href"
+    >
+      {{ item.label }}
+    </RouterLink> -->
+  </nav>
+</template>
+<script>
+export default {
+  name: "SiteHeader",
+  props: {
+    linkCollection: { type: Array, required: false },
+  },
+};
+</script>
