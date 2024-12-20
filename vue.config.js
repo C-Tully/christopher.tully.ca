39381b036619e3e36161c920135d99294a57d74f
Chris Tully <christully12@gmail.com>
2024-12-20 14:59:56 -0500

    [feature/GoogleAnalytics]: Added default tracking snippet

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
 
+const vueFeatureFlags = {
+  __VUE_OPTIONS_API__: true,
+  __VUE_PROD_DEVTOOLS__: false,
+  __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+};
+
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
@@ -9,6 +15,13 @@ module.exports = defineConfig({
         "@": path.resolve(__dirname, "src"), // Alias for src
       },
     },
+  },
+  chainWebpack: (config) => {
+    // Add feature flags using DefinePlugin
+    config.plugin("define").tap((definitions) => {
+      Object.assign(definitions[0]["process.env"], vueFeatureFlags);
+      return definitions;
+    });
   },
   css: {
     loaderOptions: {
